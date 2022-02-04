968b42bcad98e235082ddec995c76334893e05ca
Hannah Glazier <hrayglazier@aol.com>
2022-02-04 16:06:47 -0700

    commit tree 8d5c7ce9ae247e4e4157cabb20cc58415784fb7b

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 // Write your code here!
-const mainElement = document.querySelector('main').remove();
 
-const newHeader = document.createElement('h1');
-newHeader.id = 'victory';
-newHeader.setAttribute = 'h1#victory';
-newHeader.innerHTML = "Hannah is the champion";
+
+
+
+document.querySelector('main#main').remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+let yourName = "Cole Vallery"
+newHeader.textContent = `${yourName} is the champion`
+
+
+
+
+
+
+
+
+
+
+
+// const mainElement = document.querySelector('main').remove();
+
+// const newHeader = document.createElement('h1');
+// newHeader.id = 'victory';
+// newHeader.setAttribute = 'h1#victory';
+// newHeader.innerHTML = "Hannah is the champion";
+
